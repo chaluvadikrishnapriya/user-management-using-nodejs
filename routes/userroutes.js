784e9e55cc67866772ad025646a3a5f43d89e947
chaluvadikrishnapriya <chkrishnapriya2005@gmail.com>
2024-12-26 19:19:31 +0530

    initialcommit

---
+++ routes/userroutes.js
@@ -0,0 +1,9 @@
+const express=require("express");
+const mongoose=require("mongoose");
+const user=require("../model/usermodel");
+
+const router=express.Router();
+
+router.get("/",(req,res)=>{
+
+});
